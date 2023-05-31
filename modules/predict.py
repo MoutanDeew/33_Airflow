f96a5480376316feda5f3d5dcd3de6ecf1509e4e
Герман Катаев <germankataev@gmail.com>
2023-05-31 15:30:44 +0300

    fixed paths for pipline and predict files. Removed unnecessary folders and files.

--- modules/predict.py
+++ modules/predict.py
@@ -1,17 +1,20 @@
 import glob
 import json
+import os
 from datetime import datetime
 
 import dill
 import pandas as pd
 
+path = os.environ.get('PROJECT_PATH', '.')
+
 
 def predict():
-    with open('../data/models/cars_pipe_202305221126.pkl', 'rb') as file:
+    with open(f'{path}/data/models/cars_pipe_202305311142.pkl', 'rb') as file:
         best_model = dill.load(file)
 
     df_pred = pd.DataFrame(columns=['car_id', 'pred'])
-    for filename in glob.glob('../data/test/*.json'):
+    for filename in glob.glob(f'{path}/data/test/*.json'):
         with open(filename) as fin:
             form = json.load(fin)
             df = pd.DataFrame.from_dict([form])
@@ -20,7 +23,7 @@ def predict():
             df1 = pd.DataFrame(x)
             df_pred = pd.concat([df_pred, df1], axis=0)
 
-    df_pred.to_csv(f'../data/predictions/preds_{datetime.now().strftime("%m%d%y%H%M")}')
+    df_pred.to_csv(f'{path}/data/predictions/preds_{datetime.now().strftime("%m%d%y%H%M")}')
 
 
 if __name__ == '__main__':
